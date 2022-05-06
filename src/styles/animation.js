ccf30badb78c86ab05bc3a074e4c1413dbcc352e
TrakaDev <trakadev@gmail.com>
2022-05-06 12:31:39 -0500

    [ADD] Animation Styles

---
+++ src/styles/animation.js
@@ -0,0 +1,21 @@
+import { css, keyframes } from 'styled-components'
+
+export const fadeIn = ({
+  time = '1s',
+  type = 'ease'
+} = {}) =>
+  css`
+animation: ${time} ${fadeInKeyframes} ${type};
+`
+
+const fadeInKeyframes = keyframes`
+from{
+    filter: blur(5px);
+    opacity: 0;
+}
+
+to {
+    filter: blue(0);
+    opacity: 1;
+}
+`
