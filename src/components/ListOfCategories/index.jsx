ec36a7d29838883455c7e1b047a5a1cf4329702a
TrakaDev <trakadev@gmail.com>
2022-05-06 12:43:45 -0500

    [ADD] API CALL Categories

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-import db from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const url = 'https://petgram-server-traka-2fhrhuzjx-traka-dev.vercel.app/categories'
+    window.fetch(url)
+    .then(res => res.json())
+    .then(response=>{
+      setCategories(response)
+    })
+  }, [])
+
   return (
     <List>
-      {db.categories.map((category) => (
+      {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
