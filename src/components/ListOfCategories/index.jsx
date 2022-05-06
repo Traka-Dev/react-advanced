e52fa4c25a9597562023daec74e9b8a8cfc27103
TrakaDev <trakadev@gmail.com>
2022-05-06 13:51:33 -0500

    [ADD] loading categories

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -2,25 +2,55 @@ import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
 
-export const ListOfCategories = () => {
+
+const useCategoriesData = () =>{
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     const url = 'https://petgram-server-traka-2fhrhuzjx-traka-dev.vercel.app/categories'
     window.fetch(url)
     .then(res => res.json())
-    .then(response=>{
+    .then(response => {
       setCategories(response)
+      setLoading(false)
     })
   }, [])
 
-  return (
-    <List>
-      {categories.map((category) => (
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false)
+  const { categories, loading } = useCategoriesData()
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    // Evitar memory leak al limpiar los event lisener
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {loading
+      ? <Item key='loading'><Category /></Item>
+      :categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
