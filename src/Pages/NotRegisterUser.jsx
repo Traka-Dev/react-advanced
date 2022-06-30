989d59e7119bda319196546978d7bbda86af7c0a
TrakaDev <trakadev@gmail.com>
2022-06-30 01:22:35 -0500

    [ADD] register form

--- src/Pages/NotRegisterUser.jsx
+++ src/Pages/NotRegisterUser.jsx
@@ -1,13 +1,26 @@
 import React, { useContext } from "react";
 import { Context } from "../Context";
 import { UserForm } from "../components/UserForm";
+import { useRegisterMutation } from "../containers/RegisterMutation";
 
 export const NotRegisterUser = () => {
-    console.dir(":;D");
     const { activateAuth } = useContext(Context);
+    const { registerMutation, error, loading } = useRegisterMutation();
+    const onSubmit = ({ email, password }) => {
+        const input = { email, password };
+        const variables = { input };
+        registerMutation({ variables: variables }).then(activateAuth);
+    };    
+
     return (
         <>
-            <UserForm  onSubmit={activateAuth}/>
+            <UserForm
+                error={error}
+                disabled = {loading}
+                title="Registrarse"
+                onSubmit={onSubmit}
+            />            
+            <UserForm title="Iniciar Sesión" onSubmit={activateAuth} />
         </>
     );
 };
---
+++ src/containers/RegisterMutation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { gql, useMutation } from '@apollo/client'
+
+const REGISTER = gql`
+    mutation signup($input: UserCredentials!){
+        signup(input: $input)
+    }
+`
+export const useRegisterMutation = () => {
+  const [registerMutation, {error, loading}] = useMutation(REGISTER)
+
+  return { registerMutation, error, loading }
+}
