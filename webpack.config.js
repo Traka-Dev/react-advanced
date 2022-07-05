6ad7e6ffc082705e8e6163fe07f413b5bb572878
TrakaDev <trakadev@gmail.com>
2022-07-05 00:42:48 -0500

    [ADD] PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,57 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-  output: {
-    filename: 'app.bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
-      }
-    }]
-  }
+    output: {
+        filename: 'app.bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new WebpackPwaManifestPlugin({
+            name: 'Petgram - Tu app de fotos de mascotas',
+            short_name: "Petgram",
+            description: 'Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente',
+            background_color: '#fff',
+            theme_color: '#b1a',
+            icons: [{
+                src: path.resolve('src/assets/icon.png'),
+                sizes: [96, 128, 192, 256, 384, 512]
+            }]
+        }),
+        new WorkboxWebpackPlugin.GenerateSW({
+            runtimeCaching: [{
+                urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'images'
+                }
+            }, {
+                urlPattern: new RegExp('https://petgram-server-traka.vercel.app'),
+                handler: 'NetworkFirst',
+                options: {
+                    cacheName: 'api'
+                }
+            }]
+        })
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }]
+    }
 }
